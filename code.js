adc3568c6a93234fb5dcef4e6088fbf8587c68f9
BeauBallesteros472 <ballesterosbeau1218@gmail.com>
2026-08-15 11:20:58 -0700

    Added palindrome checker with buttons and palindrome.html

--- code.js
+++ code.js
@@ -153,3 +153,52 @@ function playCraps()
     {
         return Math.floor(Math.random() * 800);
     }
+
+// function to test if a word is a palindrome
+function testPalin()
+{
+    // get the value that the user entered in the textbox
+    let userInput = document.getElementById("txtInput").value.trim();
+
+    // console.log("user Input: " + userInput);
+
+    isPalindrome(userInput);
+
+    // isPalindrome(userInput) == true
+    // this statement implies == true, you could write it out
+    if(isPalindrome(userInput))
+    {
+        document.getElementById("divResult").textContent = "Yes, " + 
+        userInput + " is a palindrome."
+    }
+        else
+        {
+            document.getElementById("divResult").textContent = "No, " + 
+            userInput + " is not a palindrome."
+        }
+        
+    }
+
+function isPalindrome(wordToTest)
+{
+    // remove any spaces inside of the word(s)
+    let cleanedWord = wordToTest.replace(/\s/g, "");
+
+    cleanedWord = cleanedWord.toLowerCase();
+
+    // console.log("cleanedWord= " + cleanedWord);
+
+    // convert the cleaned string to an array
+    let arrCleaned = cleanedWord.split("");
+
+    //reverse the array contant
+    arrCleaned = arrCleaned.reverse("");
+
+    // take our reversed array and convert it back to a string
+    let reversedWord = arrCleaned.join("");
+
+    console.log("Test= " + reversedWord);
+
+    // compare if the cleaned word and the reversed words are the same
+    return cleanedWord == reversedWord;
+}
